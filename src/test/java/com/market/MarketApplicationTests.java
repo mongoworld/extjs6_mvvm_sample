770a1829ff0e284a3f0ad61a5f3354093daf8758
jgh <jgh@smartlo.net>
2017-04-20 11:22:47 +0900

    프로젝트 최초 등록

---
+++ src/test/java/com/market/MarketApplicationTests.java
@@ -0,0 +1,16 @@
+package com.market;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MarketApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
