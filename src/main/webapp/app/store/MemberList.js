ceb2449216b8b08f87fbe6d021baf9f4e63f7cdf
jgh <jgh@smartlo.net>
2017-04-24 12:58:37 +0900

    회원목록 데이터스토어 추가

---
+++ src/main/webapp/app/store/MemberList.js
@@ -0,0 +1,21 @@
+Ext.define("Study.store.MemberList",{
+	extend : 'Ext.data.Store',
+	alias : 'store.memberList',
+	storeId : 'memberList',
+	autoLoad : false,
+	fields : ['memberSeq','memberId','memberNm','memberAddr','memberRegDt'],
+	//한페이지 몇개 호출?
+	pageSize : 10,
+	proxy : {
+		type : 'ajax',
+		actionMethods : {
+			read : 'POST'
+		},
+		url : '/api/list/member',
+		reader : {
+			type : 'json',
+			rootProperty : 'data',
+			totalProperty : 'totalCount'
+		}
+	}
+})
