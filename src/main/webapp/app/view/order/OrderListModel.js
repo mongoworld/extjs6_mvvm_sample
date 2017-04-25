491a7c94fe6192e37a685d9154c328217c42766c
jgh <jgh@smartlo.net>
2017-04-25 16:16:05 +0900

    가상데이터 fields명 수정

--- src/main/webapp/app/view/order/OrderListModel.js
+++ src/main/webapp/app/view/order/OrderListModel.js
@@ -3,28 +3,28 @@ Ext.define('Study.view.order.OrderListModel', {
     alias: 'viewmodel.orderList',
     stores : {
     	orderList : {
-    		fields : ['orderNm','orderDt','orderStatus','productNm','orderAmt','orderPrice'],
+    		fields : ['memberNm','orderRegDt','orderStatus','orderNm','orderCnt','orderTotalPrc'],
         	data : [{
-        		orderNm : '홍길동',
-        		orderDt : new Date(),
-        		productNm : '지우개',
+        		memberNm : '홍길동',
+        		orderRegDt : new Date(),
+        		orderNm : '지우개',
         		orderStatus : 'Y',
-        		orderAmt : 100,
-        		orderPrice : 20000
+        		orderCnt : 100,
+        		orderTotalPrc : 20000
         	},{
-        		orderNm : '김철수',
-        		orderDt : new Date(),
-        		productNm : '샤프',
+        		memberNm : '김철수',
+        		orderRegDt : new Date(),
+        		orderNm : '샤프',
         		orderStatus : 'Y',
-        		orderAmt : 20,
-        		orderPrice : 1000
+        		orderCnt : 20,
+        		orderTotalPrc : 1000
         	},{
-        		orderNm : '이진수',
-        		orderDt : new Date(),
-        		productNm : '연필',
+        		memberNm : '이진수',
+        		orderRegDt : new Date(),
+        		orderNm : '연필',
         		orderStatus : 'N',
-        		orderAmt : 50,
-        		orderPrice : 10000
+        		orderCnt : 50,
+        		orderTotalPrc : 10000
         	}]
         }
     }
